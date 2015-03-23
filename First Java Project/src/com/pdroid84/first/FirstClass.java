525e0244454d0229a8f7ccd4ce3c236a7ec88bfa
pdroid84 <pdroid84@gmail.com>
2015-03-23 23:49:39 +0000

    second commit

--- First Java Project/src/com/pdroid84/first/FirstClass.java
+++ First Java Project/src/com/pdroid84/first/FirstClass.java
@@ -3,6 +3,6 @@ package com.pdroid84.first;
 public class FirstClass {
 
 	public static void main(String[] args) {
-	    System.out.println("Hello Eclipsed  R!");
+	    System.out.println("Hello Eclipse!");
 	  }
 }
